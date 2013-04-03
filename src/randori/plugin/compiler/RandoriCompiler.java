b8633493ab583f2a2751224c7e42e2807ab5271e
Frédéric THOMAS <fthomas@apache.org>
2013-04-03 15:02:15 +0200

    Fixed: The plugin should use the IDEA internal build system.
    Note: Unchecked your general compiler settings "Use external build" to make it work.
    
    Save all -> Parse problems
    Make -> Compile the changed files
    Rebuild -> Clean and compile all files
    Run -> Make and Launch the browser.
    
    -Removed the now useless Randori build actions.
    -Added incremental compilation.

---
+++ src/randori/plugin/compiler/RandoriCompiler.java
@@ -0,0 +1,108 @@
+/***
+ * Copyright 2013 Teoti Graphix, LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ * @author Michael Schmalle <mschmalle@teotigraphix.com>
+ */
+
+package randori.plugin.compiler;
+
+import java.util.*;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.compiler.CompileContext;
+import com.intellij.openapi.compiler.CompileScope;
+import com.intellij.openapi.compiler.TranslatingCompiler;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.Chunk;
+import randori.plugin.AsFileType;
+import randori.plugin.components.RandoriProjectComponent;
+import randori.plugin.utils.ProjectUtils;
+
+/**
+ * IDEA Compiler class for calling the internal compiler API.
+ *
+ * @author Frédéric THOMAS
+ */
+public class RandoriCompiler implements TranslatingCompiler {
+
+    private static final Logger LOG = Logger.getInstance("#randori.compiler.RandoriCompiler");
+    protected final Project myProject;
+    private FileDocumentManager m_documentManager;
+    private RandoriProjectComponent m_projectComponent;
+    private List<VirtualFile> m_unsavedFiles;
+
+    public RandoriCompiler(Project project) {
+        myProject = project;
+
+        m_projectComponent = ProjectUtils.getProjectComponent(project);
+        m_documentManager = ApplicationManager.getApplication().getComponent(FileDocumentManager.class);
+    }
+
+    @Override
+    public boolean isCompilableFile(VirtualFile file, CompileContext context) {
+        FileType fileType = file.getFileType();
+        FileType asFileType = AsFileType.AS_FILE_TYPE;
+        boolean b = file.getPath().endsWith('.' + AsFileType.DEFAULT_EXTENSION);
+        return b;
+    }
+
+    @Override
+    public void compile(CompileContext context, Chunk<Module> moduleChunk, VirtualFile[] files, OutputSink sink) {
+        context.getProgressIndicator().checkCanceled();
+        context.getProgressIndicator().setText("Starting Randori compiler...");
+
+        if (context.isMake() && m_unsavedFiles != null && m_unsavedFiles.size() > 0) {
+            m_projectComponent.build((m_unsavedFiles.toArray(new VirtualFile[m_unsavedFiles.size()])), false, true);
+            m_unsavedFiles = null;
+        } else {
+            m_projectComponent.build(!context.isMake());
+        }
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "Randori Compiler";
+    }
+
+    @Override
+    public boolean validateConfiguration(CompileScope scope) {
+        boolean isConfigurationValidated = m_projectComponent.validateConfiguration(scope);
+
+        if (isConfigurationValidated) {
+            final Document[] unsavedDocuments = m_documentManager.getUnsavedDocuments();
+
+            if (unsavedDocuments.length > 0) {
+                m_unsavedFiles = new ArrayList<VirtualFile>();
+
+                for (Document unsavedDocument : unsavedDocuments) {
+                    VirtualFile file = m_documentManager.getFile(unsavedDocument);
+                    if (file.getPath().endsWith('.' + AsFileType.DEFAULT_EXTENSION)) {
+                        m_unsavedFiles.add(file);
+                    }
+                }
+            }
+        }
+
+        return isConfigurationValidated;
+    }
+}
--- src/randori/plugin/components/BaseRandoriProjectComponent.java
+++ src/randori/plugin/components/BaseRandoriProjectComponent.java
@@ -1,17 +1,22 @@
 package randori.plugin.components;
 
+import org.apache.flex.compiler.problems.ICompilerProblem;
+import com.intellij.openapi.compiler.CompileScope;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.LogicalPosition;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.roots.ui.configuration.ClasspathEditor;
+import com.intellij.openapi.roots.ui.configuration.ModulesConfigurator;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.apache.flex.compiler.problems.ICompilerProblem;
-import randori.plugin.execution.CompilerArguments;
+import randori.compiler.clients.CompilerArguments;
+import randori.plugin.module.RandoriModuleType;
 import randori.plugin.runner.RandoriRunConfiguration;
 import randori.plugin.runner.RandoriServerComponent;
 import randori.plugin.service.ProblemsService;
@@ -19,9 +24,9 @@ import randori.plugin.utils.ProjectUtils;
 import randori.plugin.utils.VFileUtils;
 
 public class BaseRandoriProjectComponent {
+    private final RandoriBundleApplicationComponent applicationComponent;
     protected RandoriProjectModel model;
     private Project project;
-    private final RandoriBundleApplicationComponent applicationComponent;
 
     public BaseRandoriProjectComponent(Project project, RandoriBundleApplicationComponent applicationComponent) {
         this.project = project;
@@ -29,37 +34,35 @@ public class BaseRandoriProjectComponent {
         this.model = new RandoriProjectModel();
     }
 
-    public Project getProject()
-    {
+    public Project getProject() {
         return project;
     }
-    public ProblemsService getProblemsService()
-    {
+
+    public ProblemsService getProblemsService() {
         return ProblemsService.getInstance(project);
     }
 
-    public void run(RandoriRunConfiguration configuration)
-    {
-        RandoriServerComponent component = getProject().getComponent(
-                RandoriServerComponent.class);
+    public void run(RandoriRunConfiguration configuration) {
+        RandoriServerComponent component = getProject().getComponent(RandoriServerComponent.class);
         String explicitWebroot = (configuration.useExplicitWebroot) ? configuration.explicitWebroot : "";
         component.openURL(configuration.indexRoot, explicitWebroot);
     }
 
-    public void reparse(VirtualFile file)
-    {
+    public void reparse(VirtualFile file, boolean sync) {
         // parses a file and its dependencies
         // TEMP
-        parse();
+        parse(sync);
     }
 
-    public void parse()
-    {
-        CompilerArguments arguments = applicationComponent
-                .getCompilerArguments();
+    public void parse(boolean sync) {
+        CompilerArguments arguments = new CompilerArguments();
         configureDependencies(project, arguments);
 
-        applicationComponent.getBuildSourceCommand().parse(project, arguments);
+        if (sync) {
+            applicationComponent.getBuildSourceCommand().parseSync(project, arguments);
+        } else {
+            applicationComponent.getBuildSourceCommand().parse(project, arguments);
+        }
     }
 
     /**
@@ -67,14 +70,19 @@ public class BaseRandoriProjectComponent {
      *
      * @param doClean
      */
-    public void build(boolean doClean)
-    {
-        CompilerArguments arguments = applicationComponent
-                .getCompilerArguments();
-        configureDependencies(project, arguments);
+    public void build(boolean doClean) {
+        build(null, doClean, true);
+    }
 
-        applicationComponent.getBuildSourceCommand().build(project, doClean,
-                arguments);
+    public void build(VirtualFile[] files, boolean doClean, boolean sync) {
+        CompilerArguments arguments = new CompilerArguments();
+        configureDependencies(project, arguments, files);
+
+        if (sync) {
+            applicationComponent.getBuildSourceCommand().buildSync(project, doClean, arguments);
+        } else {
+            applicationComponent.getBuildSourceCommand().build(project, doClean, arguments);
+        }
     }
 
     /**
@@ -83,62 +91,78 @@ public class BaseRandoriProjectComponent {
      *
      * @param problem The ICompilerProblem to focus.
      */
-    public void openFileForProblem(ICompilerProblem problem)
-    {
+    public void openFileForProblem(ICompilerProblem problem) {
         VirtualFile virtualFile = VFileUtils.getFile(problem.getSourcePath());
-        if (virtualFile != null)
-        {
-            OpenFileDescriptor descriptor = new OpenFileDescriptor(project,
-                    virtualFile);
-            if (descriptor != null)
-            {
-                Editor editor = FileEditorManager.getInstance(project)
-                        .openTextEditor(descriptor, true);
-                if (editor != null)
-                {
-                    LogicalPosition position = new LogicalPosition(
-                            problem.getLine(), problem.getColumn());
+        if (virtualFile != null) {
+            OpenFileDescriptor descriptor = new OpenFileDescriptor(project, virtualFile);
+            if (descriptor != null) {
+                Editor editor = FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
+                if (editor != null) {
+                    LogicalPosition position = new LogicalPosition(problem.getLine(), problem.getColumn());
                     editor.getCaretModel().moveToLogicalPosition(position);
                 }
             }
         }
     }
 
-    public void configureDependencies(Project project,
-            CompilerArguments arguments)
-    {
+    public void configureDependencies(Project project, CompilerArguments arguments) {
+        configureDependencies(project, arguments, null);
+    }
+
+    public void configureDependencies(Project project, CompilerArguments arguments, VirtualFile[] virtualFiles) {
         arguments.clear();
 
-        arguments.configure(project, getModel());
+        configure(project, getModel(), arguments);
 
-        for (String library : ProjectUtils.getAllProjectSWCs(project))
-        {
+        for (String library : ProjectUtils.getAllProjectSWCs(project)) {
             arguments.addLibraryPath(library);
         }
 
-        for (String library : ProjectUtils.getAllProjectSourcePaths(project))
-        {
+        for (String library : ProjectUtils.getAllProjectSourcePaths(project)) {
             arguments.addSourcepath(library);
         }
-        
-        // save all files before parse, devs might have other files open that are
-        // not saved, IDEA works on save everything, use local history to revert paradigm I guess
-        FileDocumentManager.getInstance().saveAllDocuments();
-        
+
         Module[] modules = ModuleManager.getInstance(project).getModules();
-        for (Module module : modules)
-        {
+        for (Module module : modules) {
             // RandoriFlash/src
-            for (VirtualFile virtualFile : ModuleRootManager
-                    .getInstance(module).getSourceRoots())
-            {
-                arguments.addSourcepath(virtualFile.getPath());
+            for (VirtualFile sourceRoot : ModuleRootManager.getInstance(module).getSourceRoots()) {
+                arguments.addSourcepath(sourceRoot.getPath());
+            }
+            if (virtualFiles != null) {
+                for (VirtualFile virtualFile : virtualFiles) {
+                    arguments.addIncludedSources(virtualFile.getPath());
+                }
             }
         }
     }
 
-    public RandoriProjectModel getModel()
-    {
+    public RandoriProjectModel getModel() {
         return model;
+    }
+
+    public boolean validateConfiguration(CompileScope scope) {
+
+        if (!ProjectUtils.isSDKInstalled(project)) {
+            return false;
+        }
+
+        // TODO Implement the Randori facet for modules.
+        for (final Module module : scope.getAffectedModules()) {
+            if (ModuleType.get(module) != RandoriModuleType.getInstance()) {
+                Messages.showErrorDialog(module.getProject(), "This module is not a Randori module", "Can not compile");
+                ModulesConfigurator.showDialog(module.getProject(), module.getName(), ClasspathEditor.NAME);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void configure(Project project, RandoriProjectModel model, CompilerArguments arguments) {
+        arguments.setAppName(project.getName());
+        arguments.setJsBasePath(model.getBasePath());
+        arguments.setJsLibraryPath(model.getLibraryPath());
+        arguments.setJsOutputAsFiles(model.isClassesAsFile());
+        arguments.setOutput(project.getBasePath());
     }
 }
