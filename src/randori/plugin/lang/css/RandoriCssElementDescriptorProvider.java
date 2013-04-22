8ffaa6d506dda88153a58bdda49cfb148d703624
Roland Zwaga <roland@stackandheap.com>
2013-04-22 19:07:55 +0200

    Removed the RandoriCSSSCompletionContributor and replaced it by the RandoriCssElementDescriptorProvider, which gives a far simpler way of adding our custom completions

--- src/randori/plugin/lang/css/RandoriCssElementDescriptorProvider.java
+++ src/randori/plugin/lang/css/RandoriCssElementDescriptorProvider.java
@@ -1,7 +1,11 @@
 package randori.plugin.lang.css;
 
 import java.awt.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -11,6 +15,7 @@ import com.intellij.psi.css.CssElementDescriptorProvider;
 import com.intellij.psi.css.CssPropertyDescriptor;
 import com.intellij.psi.css.CssSimpleSelector;
 import com.intellij.psi.css.CssTerm;
+import randori.plugin.util.ProjectUtils;
 
 /**
  * @author Roland Zwaga <roland@stackandheap.com>
@@ -18,6 +23,36 @@ import com.intellij.psi.css.CssTerm;
 public class RandoriCssElementDescriptorProvider extends
         CssElementDescriptorProvider
 {
+    private static final String RANDORI_PREFIX = "-randori";
+    private static final String[] randoriNames = new String[3];
+    static
+    {
+        randoriNames[0] = RANDORI_PREFIX + "-behavior";
+        randoriNames[1] = RANDORI_PREFIX + "-mediator";
+        randoriNames[2] = RANDORI_PREFIX + "-context";
+    }
+    private static final Map<String, String> superClassLookup;
+    static
+    {
+        Map<String, String> tempMap = new HashMap<String, String>();
+        tempMap.put(randoriNames[0], "randori.behaviors.AbstractBehavior");
+        tempMap.put(randoriNames[1], "randori.behaviors.AbstractMediator");
+        tempMap.put(randoriNames[2], "guice.GuiceModule");
+        superClassLookup = Collections.unmodifiableMap(tempMap);
+    }
+    private static final Map<String, CssPropertyDescriptor> descriptorLookup;
+    static
+    {
+        Map<String, CssPropertyDescriptor> tempMap = new HashMap<String, CssPropertyDescriptor>();
+        tempMap.put(randoriNames[0], createCssPropertyDescriptor(randoriNames[0]));
+        tempMap.put(randoriNames[1], createCssPropertyDescriptor(randoriNames[1]));
+        tempMap.put(randoriNames[2], createCssPropertyDescriptor(randoriNames[2]));
+        descriptorLookup = Collections.unmodifiableMap(tempMap);
+    }
+
+    private static CssPropertyDescriptor createCssPropertyDescriptor(String randoriName) {
+        return new RandoriCssPropertyDescriptor(randoriName, superClassLookup);
+    }
 
     public RandoriCssElementDescriptorProvider()
     {
@@ -38,7 +73,7 @@ public class RandoriCssElementDescriptorProvider extends
     @Override
     public boolean isMyContext(@Nullable PsiElement context)
     {
-        return true;
+        return getBaseProvider().isMyContext(context);
     }
 
     @Nullable
@@ -46,8 +81,14 @@ public class RandoriCssElementDescriptorProvider extends
     public CssPropertyDescriptor getPropertyDescriptor(
             @NotNull String propertyName, @Nullable PsiElement context)
     {
-        return null; // To change body of implemented methods use File |
-                     // Settings | File Templates.
+        if (descriptorLookup.containsKey(propertyName))
+        {
+            return descriptorLookup.get(propertyName);
+        }
+        else
+        {
+            return getBaseProvider().getPropertyDescriptor(propertyName, context);
+        }
     }
 
     @Override
@@ -75,7 +116,13 @@ public class RandoriCssElementDescriptorProvider extends
     @Override
     public String[] getPropertyNames(@Nullable PsiElement context)
     {
-        return new String[0];
+        String[] names = getBaseProvider().getPropertyNames(context);
+        int aLen = names.length;
+        int bLen = randoriNames.length;
+        String[] propertyNames = new String[aLen+bLen];
+        System.arraycopy(names, 0, propertyNames, 0, aLen);
+        System.arraycopy(randoriNames, 0, propertyNames, aLen, bLen);
+        return propertyNames;
     }
 
     @NotNull
---
+++ src/randori/plugin/lang/css/RandoriCssPropertyDescriptor.java
@@ -0,0 +1,231 @@
+package randori.plugin.lang.css;
+
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
+import org.apache.flex.compiler.definitions.IClassDefinition;
+import org.apache.flex.compiler.definitions.ITypeDefinition;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.css.CssDeclaration;
+import com.intellij.psi.css.CssPropertyDescriptor;
+import com.intellij.psi.css.CssPropertyInfo;
+import com.intellij.psi.css.CssPropertyValue;
+import randori.compiler.access.IASProjectAccess;
+import randori.plugin.compiler.RandoriProjectCompiler;
+import randori.plugin.components.RandoriProjectComponent;
+import randori.plugin.util.ProjectUtils;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author Roland Zwaga <roland@stackandheap.com>
+ */
+public class RandoriCssPropertyDescriptor implements CssPropertyDescriptor
+{
+
+    public RandoriCssPropertyDescriptor(@NotNull String name, @NotNull Map<String, String> superClassLookup)
+    {
+        _propertyName = name;
+        _superClassLookup = superClassLookup;
+    }
+
+    final private String _propertyName;
+    final private Map<String, String> _superClassLookup;
+
+    @Nullable
+    @Override
+    public String generateDoc(PsiElement context)
+    {
+        return _propertyName + " is a Randori specific CSS property";
+    }
+
+    @Override
+    public boolean isValidValue(@NotNull PsiElement element)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isMultiValueProperty()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isShorthandValue()
+    {
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public String[] expand(@NotNull CssDeclaration declaration)
+    {
+        return new String[0];
+    }
+
+    @Nullable
+    @Override
+    public PsiElement[] getShorthandPsiValue(@NotNull CssDeclaration decl,
+            @NotNull String propertyName)
+    {
+        return new PsiElement[0];
+    }
+
+    @Override
+    public boolean is4ValueProperty()
+    {
+        return true;
+    }
+
+    @Override
+    public CssPropertyValue getReferencedPropertyValue(@NotNull String name)
+    {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public CssPropertyInfo[] getInfos()
+    {
+        return new CssPropertyInfo[0];
+    }
+
+    @NotNull
+    @Override
+    public String getPropertyName()
+    {
+        return _propertyName;
+    }
+
+    @Override
+    public boolean allowsLengthes()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean allowsPercentages()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean hasStringValue()
+    {
+        return true;
+    }
+
+    @Override
+    public Object[] getAllVariants()
+    {
+        return new Object[0];
+    }
+
+    @Nullable
+    @Override
+    public String[] getRefNames()
+    {
+        return new String[0];
+    }
+
+    @NotNull
+    @Override
+    public Object[] getVariants(@NotNull PsiElement contextElement)
+    {
+        return getSubClassesForPropertyName(_propertyName, contextElement.getProject());
+    }
+
+    private Object[] getSubClassesForPropertyName(String propertyName, Project project)
+    {
+        String superClass = getSuperClassNameForCSSDeclaration(propertyName);
+        if (superClass != null)
+        {
+            final RandoriProjectComponent projectComponent = ProjectUtils
+                    .getProjectComponent(project);
+            final RandoriProjectCompiler compiler = projectComponent
+                    .getCompiler();
+            if (compiler != null)
+            {
+                IASProjectAccess projectAccess = compiler
+                        .getProjectAccess();
+
+                if (projectAccess != null)
+                {
+                    Collection<IClassDefinition> subClasses = getSubClasses(
+                            superClass,
+                            projectAccess);
+                    if (subClasses != null)
+                    {
+                        Object[] result = new Object[subClasses.size()];
+                        int i = 0;
+                        for(IClassDefinition definition : subClasses)
+                        {
+                            result[i++] = LookupElementBuilder.create("\"" + definition.getQualifiedName() + "\"").withPresentableText(definition.getQualifiedName());
+                        }
+                        return result;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private Collection<IClassDefinition> getSubClasses(String superClass,
+                                                       IASProjectAccess projectAccess)
+    {
+        ITypeDefinition typeDefinition = projectAccess.getType(superClass);
+        if ((typeDefinition != null)
+                && (typeDefinition instanceof IClassDefinition))
+        {
+            return projectAccess
+                    .getSubClasses((IClassDefinition) typeDefinition);
+        }
+        return null;
+    }
+
+    private String getSuperClassNameForCSSDeclaration(String name)
+    {
+        if (_superClassLookup.containsKey(name))
+        {
+            return _superClassLookup.get(name);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean getInherited()
+    {
+        return false;
+    }
+
+    @NotNull
+    @Override
+    public String toCannonicalName(@NotNull String propertyName)
+    {
+        return propertyName;
+    }
+
+    @Override
+    public CssPropertyValue getValue()
+    {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public PsiElement[] getDeclarations(PsiElement context)
+    {
+        return new PsiElement[0];
+    }
+
+    @Override
+    public boolean allowsIntegerWithoutSuffix()
+    {
+        return false;
+    }
+}
